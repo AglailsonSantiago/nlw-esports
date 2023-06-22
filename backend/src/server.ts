1f86f3531cf4a1b55759410c237d0884c388a066
unknown <aglailsonsantiago@gmail.com>
2023-06-22 18:29:17 -0300

    Configuracoes iniciais para o backend node

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+    return response.json([
+        {id: 1, name: 'anucio 1'},
+        {id: 2, name: 'anucio 2'},
+        {id: 3, name: 'anucio 3'},
+        {id: 4, name: 'anucio 4'},
+    ])
+})
+
+app.listen(3333)
