c88feb882de722b68d37ae6237eddbe55056ba5b
unknown <aglailsonsantiago@gmail.com>
2023-06-27 11:19:47 -0300

    Criando endpoints iniciais da api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,35 @@
-import express from 'express'
+import express, { application, request, response } from 'express'
 
 const app = express()
 
+//listar jogos
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+//cadastrar jogo
+app.post('/games', (request, response) => {
+    return response.json([])
+})
+
+//listar anúncios de um jogo
+app.get('/games/:id/ads', (request, response) => {
+    return response.json([])
+})
+
+//cadastrar anúncio
+app.post('/ads', (request, response) => {
+    return response.json([])
+})
+
+//listar anúncios
 app.get('/ads', (request, response) => {
-    return response.json([
-        {id: 1, name: 'anucio 1'},
-        {id: 2, name: 'anucio 2'},
-        {id: 3, name: 'anucio 3'},
-        {id: 4, name: 'anucio 4'},
-    ])
+    return response.json([])
+})
+
+//listar discord de determinado anúncio
+app.get('/ads', (request, response) => {
+    return response.json([])
 })
 
 app.listen(3333)
